b0ce938ab25392dff263c513cd219a2be95313d9
Guilleha719 <145007220+Guilleha719@users.noreply.github.com>
2023-11-24 11:38:27 +0100

    Create Hylass_G_ejer1a.py

---
+++ Hylass_G_ejer1a.py
@@ -0,0 +1,12 @@
+# Este programa sirve para ...
+def programa_1 ():
+  max_num = input("Introduce un número: ")
+  sum_nums = max_num
+for i in range(9):
+  num = input("Introduce otro número ")
+  sum_nums =sum_nums+ num
+if num > max_num:
+  max_num = num
+print("The largest number is:", )
+print("The average is:", )
+programa_1()
